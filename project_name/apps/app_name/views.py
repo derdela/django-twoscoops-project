d57a8aab3d374439ccb7610892e0f56437d1b4e8
Dennis Lange <langede@uni-hildesheim.de>
2014-09-16 20:37:12 +0200

    added app template directory

---
+++ project_name/apps/app_name/views.py
@@ -0,0 +1,5 @@
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+
+# Create your views here.
